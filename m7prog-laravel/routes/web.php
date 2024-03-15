da5aab7753cf7d1aec56662e900f2049d463eee8
EnzoPaalman <35348@ma-web.nl>
2024-03-15 15:28:13 +0100

    les3 bijna af

--- m7prog-laravel/routes/web.php
+++ m7prog-laravel/routes/web.php
@@ -28,4 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/about', [ App\Http\Controllers\AboutController::class, 'index'])->name('about');
+
+
 require __DIR__.'/auth.php';
